1cc4815c85b4284309fc364b0b661b2dca35dafa
uncsmDev <uncsm_devsistem@outlook.com>
2024-05-14 11:35:49 -0600

    Material css
    Instalación de paquete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,14 @@ import { OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { initFlowbite } from 'flowbite';
 
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
+
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,MatButtonModule, MatMenuModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
