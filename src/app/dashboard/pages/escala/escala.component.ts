bb04017f519e8c9c37b58ece124c9d65a46be239
Luis Espinoza <luis.manuel.espinoza.estrada@gmail.com>
2024-05-23 15:10:04 -0600

    mejora

---
+++ src/app/dashboard/pages/escala/escala.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-escala',
+  standalone: true,
+  imports: [],
+  templateUrl: './escala.component.html',
+  styleUrl: './escala.component.css'
+})
+export class EscalaComponent {
+
+}
