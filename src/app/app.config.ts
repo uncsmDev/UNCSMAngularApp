90e2dad007f6822cc38f8946b8c96b20b3f69f32
Luis Espinoza <luis.manuel.espinoza.estrada@gmail.com>
2024-06-03 08:36:32 -0600

    cambios

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,11 +5,13 @@ import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { tokenHttpInterceptor } from './token-http-interceptor';
+import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes, withViewTransitions({skipInitialTransition: true})), 
     provideAnimationsAsync(),
-    provideHttpClient(withInterceptors([tokenHttpInterceptor]))
+    provideHttpClient(withInterceptors([tokenHttpInterceptor])),
+    provideClientHydration()
   ]
 };
