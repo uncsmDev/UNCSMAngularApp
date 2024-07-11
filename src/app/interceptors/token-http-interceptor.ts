ddc46a257d7d2a3f511b9f41c0b3162ecc161be0
Luis Espinoza <luis.manuel.espinoza.estrada@gmail.com>
2024-07-11 11:15:18 -0600

    cambios

--- src/app/interceptors/token-http-interceptor.ts
+++ src/app/interceptors/token-http-interceptor.ts
@@ -2,26 +2,18 @@ import { HttpInterceptorFn } from "@angular/common/http";
 import { LoginResult } from "@interfaces/acount";
 
 export const tokenHttpInterceptor: HttpInterceptorFn = (req, next) => {
+    debugger
     const tokenString = sessionStorage.getItem("loggedInUser");
     const token: LoginResult = tokenString ? JSON.parse(tokenString) : null;
-
+    console.log(req)
     if(token !== null)
     {
-        req = req.clone({
-            setHeaders:{
-                'Authorization':'Bearer ' + token.token
-            }
-        });
+        req = req.clone({setHeaders:{'Authorization':'Bearer ' + token.token}});
         sessionStorage.setItem("isAuth", "true");
     }else
     {
-        req = req.clone({
-            setHeaders:{
-               'Authorization':'Bearer ' + "ninguno"
-            }
-        });
         sessionStorage.setItem("isAuth", "false");
     }
-    
+    console.log(req)
     return next(req);
 }
