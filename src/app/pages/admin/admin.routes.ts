ed3367a37ae5f99a7a8f2865f92ea719577682c6
Luis Espinoza <luis.manuel.espinoza.estrada@gmail.com>
2024-11-26 09:58:02 -0600

    Reorganizando rutas en cada modulo

---
+++ src/app/pages/admin/admin.routes.ts
@@ -0,0 +1,72 @@
+import { Routes } from "@angular/router";
+import { routerPermisseGuard } from "app/guards/router-permisse.guard";
+
+export default [
+    {
+        path: '',
+        redirectTo: '/admin/home',
+        pathMatch: 'full'
+      },
+    {
+        path:'usuarios', 
+        title: 'Usuarios',
+        loadComponent:()=>import('../../auth/usuarios/usuarios.component')
+    },
+ 
+    {
+        path:'dependencias', 
+        title: 'Dependencias',
+        loadComponent:()=>import('./dependencias/dependencias.component')
+    },
+    {
+        path: 'home',
+        title: 'Home',
+        loadComponent: () => import('./home/home.component'),
+    },
+    {
+        path: 'modulo',
+        title: 'Catalogo Modulo',
+        loadComponent: () => import('./modulo/modulo.component'),
+    },
+    
+    {
+        path: 'submodulo',
+        title: 'Catalogo SubModulos',
+        loadComponent: () => import('./submodulo/submodulo.component'),
+    },
+    {
+        path: 'trabajador',
+        title: 'Trabajadores',
+        loadComponent: () => import('./trabajador/trabajador.component'),
+    },
+    {
+        path: 'trabajador/nuevo-trabajador',
+        title: 'Nuevo Trabajador',
+        loadComponent: () => import('./trabajador/trabajador-input/trabajador-input.component'),
+    },
+    {
+        path:'trabajador/usuario/:id', 
+        title: 'Usuario',
+        loadComponent:()=>import('../../auth/usuarios/usuario/usuario.component')
+    },
+    {
+        path: 'perfilEntidad/:id',
+        title: 'Perfil',
+        loadComponent: () => import('./perfilEntidad/perfil.entidad.component'),
+    },
+    {
+        path: 'trabajador/perfil/:id',
+        title: 'Perfil Trabajador',
+        loadComponent: () => import('./trabajador/trabajador-perfil/trabajador-perfil.component'),
+    },
+    {
+        path: 'contratos',
+        title: 'Contratos',
+        loadComponent: () => import('./contratos/contratos.component'),
+    },
+    {
+        path: 'contratos/contrato/:id',
+        title: 'Contrato Perfil',
+        loadComponent: () => import('./contratos/contrato/contrato.component'),
+    }
+] as Routes;
---
+++ src/app/pages/sed/sed.routes.ts
@@ -0,0 +1,89 @@
+import { Routes } from "@angular/router";
+import { routerPermisseGuard } from "app/guards/router-permisse.guard";
+
+export default [
+    {
+        path: '',
+        redirectTo: '/sed/home',
+        pathMatch: 'full',
+      },
+    {
+        path: 'home',
+        title: 'Home',
+        loadComponent: () => import('./home/home.component'),
+        data: { breadcrumb: 'Home' },
+        canActivate: [routerPermisseGuard]
+    },
+      {
+        path: 'evaluacion/:id',
+        title: 'Aplicación de la Evaluación',
+        loadComponent: () => import('.//home/AplicacionEvaluacion/AplicacionEvaluacion.component'),
+        data: { breadcrumb: 'Escalas' },
+        canActivate: [routerPermisseGuard]
+      },
+      {
+        path: 'personal',
+        title: 'Personal',
+        loadComponent: () => import('./home/personal/personal.component'),
+        data: { breadcrumb: 'Escalas' },
+        canActivate: [routerPermisseGuard]
+      },
+    {
+        
+        path: 'escala',
+        title: 'Escalas',
+        loadComponent: () => import('./escala/escala.component'),
+        data: { breadcrumb: 'Escalas' },
+        canActivate: [routerPermisseGuard]
+    },
+    {
+        
+        path: 'periodo',
+        title: 'Periodos',
+        loadComponent: () => import('./periodo/periodo.component'),
+        data: { breadcrumb: 'Periodos' },
+        canActivate: [routerPermisseGuard]
+    },
+    {
+        path: 'tipos-evaluaciones',
+        title: 'Tipos de Evaluaciones',
+        loadComponent: () => import('./instrumento/TipoEvaluacion/TipoEvaluacion.component'),
+        data: { breadcrumb: 'Tipos de Evaluaciones' },
+        canActivate: [routerPermisseGuard]
+    },
+    {
+        path: 'tipo-trabajador/:id',
+        title: 'Tipos de trabajadores',
+        loadComponent: () => import('./instrumento/tipo-trabajador/TipoTrabajador.component'),
+        data: { breadcrumb: 'Tipos de trabajadores' },
+        canActivate: [routerPermisseGuard],
+    },
+    {
+        path: 'instrumentos/:TipoEvaluacionId/:TipoTrabajadorId',
+        title: 'Instrumentos',
+        loadComponent: () => import('./instrumento/instrumento.component'),
+        data: { breadcrumb: 'Instrumentos' },
+        canActivate: [routerPermisseGuard]
+    }
+    ,{
+        path: 'pc/:id',
+        title: 'Preguntas Cerradas',
+        loadComponent: () => import('./instrumento/preguntas-cerradas/preguntas.component'),
+        data: { breadcrumb: 'Preguntas Cerradas' },
+        canActivate: [routerPermisseGuard]
+    },
+    {
+        path: 'pa/:id',
+        title: 'Preguntas Abiertas',
+        loadComponent: () => import('./instrumento/preguntas-abiertas/preguntas-abiertas.component'),
+        data: { breadcrumb: 'Preguntas Abiertas' },
+        canActivate: [routerPermisseGuard]
+    },
+    {
+        path: 'asignacion/:id/:dependenciaId',
+        title: 'Asignación para la Evaluación',
+        loadComponent: () => import('./asignacion-evaluacio/asignacion/asignacion.component'),
+        data: { breadcrumb: 'Asignación para la Evaluación' },
+        canActivate: [routerPermisseGuard]
+    }
+] as Routes;
